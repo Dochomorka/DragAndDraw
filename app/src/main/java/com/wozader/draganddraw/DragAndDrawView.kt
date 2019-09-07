1d0edf211a4c2d008f0d4bb7d219813957e0189a
Yimesgen Morka <webdesigner299@gmail.com>
2019-09-07 12:09:05 +0300

    Added New Custom DragAndDrawView by extending the base class View and Replace Android studio default Root Layout (Constraint Layout ) with the custom View(DragAndDrawView) as a Root Element in activity_drag_and_draw.xml

---
+++ app/src/main/java/com/wozader/draganddraw/DragAndDrawView.kt
@@ -0,0 +1,11 @@
+package com.wozader.draganddraw
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class DragAndDrawView(context: Context, attributeSet: AttributeSet? = null): View(context,attributeSet) {
+    init {
+
+    }
+}
