9a66553b1070913ed3cc4d3cf0419db5830ed3e9
Yimesgen Morka <webdesigner299@gmail.com>
2019-09-07 12:46:00 +0300

    implement onTouchEvent() inside DragAndDrawView make DragAndDrawActivity Launcher activity

--- app/src/main/java/com/wozader/draganddraw/DragAndDrawView.kt
+++ app/src/main/java/com/wozader/draganddraw/DragAndDrawView.kt
@@ -1,11 +1,34 @@
 package com.wozader.draganddraw
 
 import android.content.Context
+import android.graphics.PointF
 import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 
 class DragAndDrawView(context: Context, attributeSet: AttributeSet? = null): View(context,attributeSet) {
+    val TAG = "DragAndDrawView"
+
+    private val ACTION_DOWN = "ACTION_DOWN"
+    private val ACTION_UP = "ACTION_UP"
+    private val ACTION_CANCEL = "ACTION_CANCEL";
+    private val ACTION_MOVE = "ACTION_MOVE"
+
     init {
 
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val current = PointF(event?.x!!, event?.y!!)
+        var action = when(event.action){
+            MotionEvent.ACTION_DOWN -> ACTION_DOWN
+            MotionEvent.ACTION_UP -> ACTION_UP
+            MotionEvent.ACTION_CANCEL -> ACTION_CANCEL
+            MotionEvent.ACTION_MOVE -> ACTION_MOVE
+            else-> ""
+        }
+        Log.i(TAG, "$action at x = ${current.x}, y = ${current.y}")
+        return true
     }
 }
